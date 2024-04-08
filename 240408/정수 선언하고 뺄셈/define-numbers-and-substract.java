59e9b60656d70b130424d499b06ea94a9b2ef768
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-04-08 14:56:00 +0900

    Title: 정수 선언하고 뺄셈, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240408/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 97;
+        b = 13;
+        System.out.print(a);
+        System.out.print(" - ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.print(a-b);
+    }
+}
