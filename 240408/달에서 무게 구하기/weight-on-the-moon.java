d5af5db19ee5e796c8bfe83499b0cc0ef90cbae6
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-04-08 15:44:28 +0900

    Title: 달에서 무게 구하기, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240408/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 13;
+        double gravity = 0.165;
+        System.out.printf("%d * %f = %.6f",weight,gravity,weight*gravity);
+    }
+}
