cf7e0145be92f7515b3bd7939f0a9bf349241b49
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-04-08 14:48:51 +0900

    Title: 2줄 출력, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 240408/2줄 출력/print-two-lines.java
+++ 240408/2줄 출력/print-two-lines.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello")
-        System.out.println("World")
+        System.out.println("Hello");
+        System.out.println("World");
     }
 }
