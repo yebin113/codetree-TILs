34f89726ba4375c8c6c605dddcb581d61a8bfa2f
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-04-09 23:16:50 +0900

    Title: 실수 입력받아 계산, Time: 135ms, Memory: 11MB, Status: Passed - CodeTree

--- 240409/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
+++ 240409/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextDouble();
-        int b = sc.nextDouble();
-        System.out.printf("%.2f",(a+b));
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.printf("%.2f",a+b);
     }
 }
