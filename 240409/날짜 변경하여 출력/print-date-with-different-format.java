0537245035b4c839237b537518a5b0704182bac9
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-04-09 23:56:06 +0900

    Title: 날짜 변경하여 출력, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/날짜 변경하여 출력/print-date-with-different-format.java
+++ 240409/날짜 변경하여 출력/print-date-with-different-format.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
-        sc.useDelimiter("."); // - 기준으로 잘라서 입력 받겠다는 뜻
-        int a = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
-        int b = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
-        int c = sc.nextInt(); // - 기호가 나오기 전 or 입력의 마지막 부분까지 진행
-        System.out.printf("%d-%d-%d",b,c,a);
+        sc.useDelimiter("\\."); // - 기준으로 잘라서 입력 받겠다는 뜻
+        String a = sc.next(); // - 기호가 나오기 전까지 입력 진행
+        String b = sc.next(); // - 기호가 나오기 전까지 입력 진행
+        String c = sc.next(); // - 기호가 나오기 전 or 입력의 마지막 부분까지 진행
+        System.out.printf("%s-%s-%s",b,c,a);
     }
 }
