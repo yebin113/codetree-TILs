1b0c48677446ed0fb5ec3931fe92e1fa3481a57f
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-04-09 23:16:21 +0900

    Title: 실수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240409/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextDouble();
+        int b = sc.nextDouble();
+        System.out.printf(".2f",(a+b));
+    }
+}
