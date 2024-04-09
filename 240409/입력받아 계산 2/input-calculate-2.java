3e574d8d96ca89518d57c1fdb04934a7c59beeb4
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-04-09 17:50:35 +0900

    Title: 입력받아 계산 2, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240409/입력받아 계산 2/input-calculate-2.java
+++ 240409/입력받아 계산 2/input-calculate-2.java
@@ -5,6 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(a+b);
+        System.out.println(a*b);
     }
 }
