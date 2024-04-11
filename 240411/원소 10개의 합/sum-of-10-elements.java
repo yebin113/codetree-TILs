cbe33549d4145d4a9c6ab063ec6166364185ddda
yebin113 <139421100+yebin113@users.noreply.github.com>
2024-04-11 15:12:04 +0900

    Title: 원소 10개의 합, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240411/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 10; i++){
+            int a = sc.nextInt();
+            sum += a;
+        }
+        System.out.print(sum);
+    }
+}
